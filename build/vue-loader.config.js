84679d853f0f2703cb3e5000d2e2ba48556d185d
tao <2098613620@qq.com>
2022-09-10 12:15:30 +0800

    1

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -1,9 +1,13 @@
 module.exports = (isDev) => {
   return {
+    // 消除模板空格的影响
     preserveWhitepace: true,
+    // 单独打包 css
     extractCSS: !isDev,
     cssModules: {
+      // css 打包命名方式
       localIdentName: isDev ? '[path]-[name]-[hash:base64:5]' : '[hash:base64:5]',
+      // 转化 css - 的命名方式为驼峰
       camelCase: true
     },
     // hotReload: false, // 根据环境变量生成
